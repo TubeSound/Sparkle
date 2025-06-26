7c31d8709aa0f37eee9b8fc055ebb8fa3971d95a
Tube <docs910@gmail.com>
2025-06-26 23:05:47 +0900

    bokeh ->vsixグラフ表示に変更

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { MockChartContainer } from './components/MockChartContainer';
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Visx チャートテスト</h1>
+      <MockChartContainer />
+    </div>
+  );
+}
+
+export default App;
